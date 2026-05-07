c33941c430bb047970f50cdaa25110a460531021
yunah100204 <yunah100204@gmail.com>
2026-05-07 15:17:45 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.titie('나의 첫 웹 서비스 만들기!')
+a=st.text_input('이름을 입력해주세요!')
+b=st.selectbox('좋아하는 음식을 선택하세요!',['튀김','햄버거','과자']
+if st.button('인사말 생성'):
+  st.write(a+'님!, 안녕하세요, 반갑습니다!')            
