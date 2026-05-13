6e8b200a87cb8d24ec58cb017ea2875f2bb9501f
yunah100204 <yunah100204@gmail.com>
2026-05-13 13:57:14 +0900

    Update greeting and add food preference messages
    Refactor greeting message and add food preference response.

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ st.titie('나의 첫 웹 서비스 만들기!')
 a=st.text_input('이름을 입력해주세요!')
 b=st.selectbox('좋아하는 음식을 선택하세요!',['튀김','햄버거','과자']
 if st.button('인사말 생성'):
-  st.write(a+'님!, 안녕하세요, 반갑습니다!')            
+  st.write(a+'님!, 안녕하세요!')           
+st.info('반갑습니다')
+st.warning(b+'음식을 좋아하시는군요')
+st.error('잘 부탁해요!')
+st.balloons()
